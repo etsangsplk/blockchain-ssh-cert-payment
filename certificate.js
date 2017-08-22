e63f695aebc2b9095f146759068b329ab6ad6918
Dongmin Lee <reoim10@gmail.com>
2017-08-22 23:33:42 +0900

    performance test

--- certificate.js
+++ certificate.js
@@ -7,6 +7,9 @@ const path = require('path')
 const util = require('util')
 const grpc = require('grpc')
 const bmt = require('./bmt/bmt')
+const winston = require('winston')
+const fs = require('fs')
+
 const peer_proto = grpc.load(PROTO_PATH).peer
 
 let options = {
@@ -34,8 +37,8 @@ let options = {
     // 10.178.195.148    peer14.bank.kfbbmt.com
     // 10.178.195.149    peer15.bank.kfbbmt.com
     // 10.178.195.150    peer16.bank.kfbbmt.com
-    network_url: 'grpc://10.178.10.147:7051',
-    endorser_url: ['grpc://10.178.10.162:7051',
+    network_url: 'grpc://10.178.10.162:7051',
+    endorser_url: ['grpc://10.178.10.147:7051',
         'grpc://10.178.10.177:7051',
         'grpc://10.178.10.181:7051',
         'grpc://10.178.10.183:7051',
@@ -52,9 +55,49 @@ let options = {
         'grpc://10.178.195.149:7051',
         'grpc://10.178.195.150:7051'
     ],
-    event_url: 'grpc://10.178.10.147:7053',
+    event_url: 'grpc://10.178.10.162:7053',
     orderer_url: 'grpc://10.178.10.131:7050'
 }
+
+// test only
+// function saveCertificates(call, callback) {
+//     let channel = {}
+//     let client = null
+//     let targets = []
+//     let tx_id = null
+//     let data = {
+//         result: true,
+//         errorMsg: ''
+//     }
+//     Promise.resolve().then(() => {
+//         console.log("Create a client and set the wallet location")
+//         client = new hfc()
+//         return hfc.newDefaultKeyValueStore({ path: options.wallet_path })
+//     }).then((wallet) => {
+//         console.log("Set wallet path, and associate user ", options.user_id, " with application")
+//         client.setStateStore(wallet)
+//         return client.getUserContext(options.user_id, true)
+//     }).then((user) => {
+//         console.log("Check user is enrolled, and set a query URL in the network")
+//         if (user === null) {
+//             console.error("User not defined, or not enrolled - error")
+//             data.result = false
+//             data.errorMsg = 'User not defined, or not enrolled - error'
+//         }
+//         channel = client.newChannel(options.channel_id)
+//         let peerObj = client.newPeer(options.network_url)
+//         channel.addPeer(peerObj)
+//         channel.addOrderer(client.newOrderer(options.orderer_url))
+//         targets.push(peerObj)
+//         callback(null, data)
+//         return
+//     }).catch((err) => {
+//         data.result = false
+//         data.errorMsg = err
+//         callback(null, data)
+//         console.error("Caught Error", err)
+//     })
+// }
 
 function saveCertificate(call, callback) {
     let data = {
@@ -92,9 +135,151 @@ function saveCertificate(call, callback) {
 
 function saveCertificates(call, callback) {
     let data = {
-        result: false,
-        errorMsg: ''
-    }
+            result: false,
+            errorMsg: ''
+        }
+        /*
+            let channel = {}
+            let client = null
+            let targets = []
+            let tx_id = null
+            let clientPromise = new Promise((resolve, reject) => {
+                if (client != null) {
+                    reject('test purpose only')
+                } else {
+                    resolve()
+                }
+            })
+            clientPromise.then(() => {
+                console.log("Create a client and set the wallet location")
+                client = new hfc()
+                return hfc.newDefaultKeyValueStore({ path: options.wallet_path })
+            }).then((wallet) => {
+                console.log("Set wallet path, and associate user ", options.user_id, " with application")
+                client.setStateStore(wallet)
+                return client.getUserContext(options.user_id, true)
+            }).then((user) => {
+                console.log("Check user is enrolled, and set a query URL in the network")
+                if (user === null) {
+                    console.error("User not defined, or not enrolled - error")
+                }
+                channel = client.newChannel(options.channel_id)
+                let peerObj = client.newPeer(options.network_url)
+                channel.addPeer(peerObj)
+                channel.addOrderer(client.newOrderer(options.orderer_url))
+                targets.push(peerObj)
+                options.endorser_url.forEach((endorser) => {
+                    targets.push(client.newPeer(endorser))
+                })
+                return
+            }).then(() => {
+                tx_id = client.newTransactionID()
+                console.log("Assigning transaction_id: ", tx_id._transaction_id)
+                let fcn = 'createCertificates'
+                let args = [call.request.certificateSets]
+                console.log('query fcn: ', fcn)
+                console.log('query args: ', args)
+
+                // send proposal to endorser
+                let request = {
+                    targets: targets,
+                    chaincodeId: options.chaincode_id,
+                    fcn: fcn,
+                    args: args,
+                    chainId: options.channel_id,
+                    txId: tx_id
+                }
+
+                return channel.sendTransactionProposal(request)
+            }).then((results) => {
+                let proposalResponses = results[0]
+                let proposal = results[1]
+                let header = results[2]
+                let isProposalGood = false
+                if (proposalResponses && proposalResponses[0].response &&
+                    proposalResponses[0].response.status === 200) {
+                    isProposalGood = true
+                    console.log('Transaction proposal was good')
+                } else {
+                    console.error('Transaction proposal was bad')
+                }
+                if (isProposalGood) {
+                    console.log(util.format(
+                        'Successfully sent Proposal and received ProposalResponse: Status - %s, message - "%s", metadata - "%s", endorsement signature: %s',
+                        proposalResponses[0].response.status, proposalResponses[0].response.message,
+                        proposalResponses[0].response.payload, proposalResponses[0].endorsement.signature))
+                    let request = {
+                            proposalResponses: proposalResponses,
+                            proposal: proposal,
+                            header: header
+                        }
+                        // set the transaction listener and set a timeout of 30sec
+                        // if the transaction did not get committed within the timeout period,
+                        // fail the test
+                    let transactionID = tx_id.getTransactionID()
+                    let eventPromises = []
+                    let eh = client.newEventHub()
+                    eh.setPeerAddr(options.event_url)
+                    eh.connect()
+
+                    let txPromise = new Promise((resolve, reject) => {
+                        let handle = setTimeout(() => {
+                            eh.disconnect()
+                            reject()
+                        }, 30000)
+
+                        eh.registerTxEvent(transactionID, (tx, code) => {
+                            clearTimeout(handle)
+                            eh.unregisterTxEvent(transactionID)
+                            eh.disconnect()
+
+                            if (code !== 'VALID') {
+                                console.error('The transaction was invalid, code = ' + code)
+                                reject()
+                            } else {
+                                console.log('The transaction has been committed on peer ' + eh._ep._endpoint.addr)
+                                resolve()
+                            }
+                        })
+                    })
+                    eventPromises.push(txPromise)
+                    let sendPromise = channel.sendTransaction(request)
+                    return Promise.all([sendPromise].concat(eventPromises)).then((results) => {
+                        console.log(' event promise all complete and testing complete')
+                        let result = results[0]
+                        result.payload = proposalResponses[0].response.payload
+                        return result // the first returned value is from the 'sendPromise' which is from the 'sendTransaction()' call
+                    }).catch((err) => {
+                        console.error(
+                            'Failed to send transaction and get notifications within the timeout period.'
+                        )
+                        return 'Failed to send transaction and get notifications within the timeout period.'
+                    })
+                } else {
+                    console.error(
+                        'Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...'
+                    )
+                    return 'Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...'
+                }
+            }).then((response) => {
+                console.log("invoke response: ", response)
+                if (response.status === 'SUCCESS') {
+                    console.log('Successfully sent transaction to the orderer.')
+                    data.result = true
+                    callback(null, data)
+                } else {
+                    console.error('Failed to order the transaction.')
+                    data.result = false
+                    data.errorMsg = response
+                    callback(null, data)
+                }
+            }).catch((err) => {
+                data.result = false
+                data.errorMsg = err
+                callback(null, data)
+                console.error("Caught Error :", err)
+            })
+        */
     bmt.invoke(options,
         'createCertificates', [call.request.certificateSets]
     ).then((response) => {
